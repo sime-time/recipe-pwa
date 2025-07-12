42c564e8c31da7b0a49ca3e591201ae19cc71faa
Dunn, Simeon <simedunn01@gmail.com>
2025-07-12 15:46:24 -0400

    pwa images

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,24 @@
+<script setup>
+// If you want to use it in setup, import from the nuxtApp.
+const { $pwa } = useNuxtApp()
+
+const toast = useToast()
+
+onMounted(() => {
+  if ($pwa.offlineReady)
+    toast.success('App ready to work offline')
+})
+</script>
+
 <template>
-  <h1>home</h1>
+  <!-- You can use $pwa directly in templates! -->
+  <div v-show="$pwa.needRefresh">
+    <span>
+      New content available, click on reload button to update.
+    </span>
+
+    <button @click="$pwa.updateServiceWorker()">
+      Reload
+    </button>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,9 +36,44 @@ export default defineNuxtConfig({
   fonts: {
     defaults: {
       weights: [400, 700],
-    }
+    },
   },
   pwa: {
+    injectRegister: "auto",
     registerType: "autoUpdate",
-  }
-})
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+    },
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg", "favicon.svg"],
+    manifest: {
+      name: 'Recipe App',
+      short_name: 'Recipes',
+      description: 'My Favorite Recipes Saved',
+      theme_color: '#F3E5F5',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+  },
+});
